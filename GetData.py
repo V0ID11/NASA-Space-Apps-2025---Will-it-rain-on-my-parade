4c2064ce888c7ff787fcca9cc9686ee914a84873
ndg1g24 <ndg1g24@soton.ac.uk>
2025-10-04 11:40:43 +0100

    Working setup

--- GetData.py
+++ GetData.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     TOKEN = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2IjoxLCJ1c2VyIjoiZ3VybmV5X25pY2hvbGFzIiwiaXNzIjoibG9naW4ubWV0ZW9tYXRpY3MuY29tIiwiZXhwIjoxNzU5NTczNDUwLCJzdWIiOiJhY2Nlc3MifQ.moa33aZJnX0MFI7uIebrGntK5VIgfHKVlD3PmIBPyAyocVxSN0RPXhw_GaVywX1esboVrCv_MmmCTUDJ9ORusw'
 
     USERNAME = 'gurney_nicholas' 
-    PASSWORD = 'y51m14ABCgT7m4F6l235'
+    PASSWORD = 'y51m14AbCgT7m4F6l235'
     get_Token(USERNAME, PASSWORD)
 
     #URL = f'https://api.meteomatics.com/2020-10-04T00Z--2025-10-04T00Z/precip_24h:mm/51.11,10.2/html?access_token={TOKEN}'
